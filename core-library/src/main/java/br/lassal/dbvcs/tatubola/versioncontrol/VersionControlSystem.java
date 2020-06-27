5be5fcade2ee489ef6b4286b732ce3fe660e8672
lassal <lucalves33@yahoo.com.br>
2020-06-27 14:44:24 -0300

    Closes feature #6 : create common interface to interact to VCS it was designed thinking in Git and SVN.

--- core-library/src/main/java/br/lassal/dbvcs/tatubola/versioncontrol/VersionControlSystem.java
+++ core-library/src/main/java/br/lassal/dbvcs/tatubola/versioncontrol/VersionControlSystem.java
@@ -1,4 +1,12 @@
 package br.lassal.dbvcs.tatubola.versioncontrol;
 
 public interface VersionControlSystem {
+
+    void setupRepositoryInitialState();
+
+    void checkout(String branch);
+
+    void commitAllChanges(String changeMessage);
+
+    void syncChangesToServer();
 }
