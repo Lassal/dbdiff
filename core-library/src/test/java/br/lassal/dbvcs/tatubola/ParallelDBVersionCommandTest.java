61946c659dacedf66f95a684b4b883f06536b665
lassal <lucalves33@yahoo.com.br>
2020-08-03 20:56:58 -0300

    Implemented test to compare that the results produced by the parallel serialization process are identical to the serial process. It closes #29

--- core-library/src/test/java/br/lassal/dbvcs/tatubola/ParallelDBVersionCommandTest.java
+++ core-library/src/test/java/br/lassal/dbvcs/tatubola/ParallelDBVersionCommandTest.java
@@ -1,7 +1,8 @@
 package br.lassal.dbvcs.tatubola;
 
-import br.lassal.dbvcs.tatubola.builder.ParallelDBVersionCommandBuilder;
+import br.lassal.dbvcs.tatubola.builder.DBModelSerializerBuilder;
 import br.lassal.dbvcs.tatubola.builder.MockDatabaseSerializerFactory;
+import br.lassal.dbvcs.tatubola.builder.ParallelDBVersionCommandBuilder;
 import br.lassal.dbvcs.tatubola.fs.InMemoryTestDBModelFS;
 import br.lassal.dbvcs.tatubola.relationaldb.model.*;
 import br.lassal.dbvcs.tatubola.relationaldb.repository.InMemoryTestRepository;
@@ -11,13 +12,13 @@ import br.lassal.dbvcs.tatubola.versioncontrol.VersionControlSystem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.Any;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.atMostOnce;
-
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParallelDBVersionCommandTest {
@@ -49,12 +50,12 @@ public class ParallelDBVersionCommandTest {
 
         ParallelDBVersionCommand cmd =
                 new ParallelDBVersionCommandBuilder(factory)
-                .setVCSRemoteInfo(remoteRepoUrl, baseBranch, username, password)
-                .setWorkspaceInfo(rootPath, tmpPath)
-                .setDBSchemasToBeSerialized(schemas)
-                .addDBEnvironment("DEV","jdbc:dummy:dev", username, password)
-                .addDBEnvironment("QA","jdbc:dummy:qa", username, password)
-                .build();
+                        .setVCSRemoteInfo(remoteRepoUrl, baseBranch, username, password)
+                        .setWorkspaceInfo(rootPath, tmpPath)
+                        .setDBSchemasToBeSerialized(schemas)
+                        .addDBEnvironment("DEV", "jdbc:dummy:dev", username, password)
+                        .addDBEnvironment("QA", "jdbc:dummy:qa", username, password)
+                        .build();
 
         cmd.takeDatabaseSchemaSnapshotVersion();
 
@@ -69,7 +70,7 @@ public class ParallelDBVersionCommandTest {
         // Test tables serialized and deserialized
         List<Table> bTables = repository.getInnerTables(schemas[1]);
 
-        for(Table sourceTable: bTables){
+        for (Table sourceTable : bTables) {
             InMemoryTestDBModelFS.SerializationInfo serializedTableInfo = outputFS.getSerializationInfo(sourceTable);
             Table deserializedTable = textSerializer.fromYAMLtoPOJO(serializedTableInfo.getYamlText(), Table.class);
 
@@ -79,7 +80,7 @@ public class ParallelDBVersionCommandTest {
         // Test views serialized and deserialized
         List<View> aViews = repository.getInnerViews(schemas[0]);
 
-        for(View sourceView: aViews){
+        for (View sourceView : aViews) {
             InMemoryTestDBModelFS.SerializationInfo serializedInfo = outputFS.getSerializationInfo(sourceView);
             View deserializedView = textSerializer.fromYAMLtoPOJO(serializedInfo.getYamlText(), View.class);
 
@@ -90,7 +91,7 @@ public class ParallelDBVersionCommandTest {
         // Test indexes serialized and deserialized
         List<Index> bIndexes = repository.getInnerIndexes(schemas[1]);
 
-        for(Index sourceIndex: bIndexes){
+        for (Index sourceIndex : bIndexes) {
             InMemoryTestDBModelFS.SerializationInfo serializedInfo = outputFS.getSerializationInfo(sourceIndex);
             Index deserializedIndex = textSerializer.fromYAMLtoPOJO(serializedInfo.getYamlText(), Index.class);
 
@@ -100,7 +101,7 @@ public class ParallelDBVersionCommandTest {
         // Test triggers serialized and deserialized
         List<Trigger> cTriggers = repository.getInnerTriggers(schemas[2]);
 
-        for(Trigger sourceTrigger: cTriggers){
+        for (Trigger sourceTrigger : cTriggers) {
             InMemoryTestDBModelFS.SerializationInfo serializedInfo = outputFS.getSerializationInfo(sourceTrigger);
             Trigger deserializedTrigger = textSerializer.fromYAMLtoPOJO(serializedInfo.getYamlText(), Trigger.class);
 
@@ -111,7 +112,7 @@ public class ParallelDBVersionCommandTest {
         // Test routines serialized and deserialized
         List<Routine> aRoutines = repository.getInnerRoutines(schemas[0]);
 
-        for(Routine sourceRoutine: aRoutines){
+        for (Routine sourceRoutine : aRoutines) {
             InMemoryTestDBModelFS.SerializationInfo serializedInfo = outputFS.getSerializationInfo(sourceRoutine);
             Routine deserializedRoutine = textSerializer.fromYAMLtoPOJO(serializedInfo.getYamlText(), Routine.class);
 
@@ -119,5 +120,94 @@ public class ParallelDBVersionCommandTest {
             assertEquals(sourceRoutine, deserializedRoutine);
         }
 
+    }
+
+    @Test
+    public void testParallelVsSerialDatabaseSchemaSnapshotVersionEquality() throws Exception {
+
+        String remoteRepoUrl = "remoteVCSUrl";
+        String baseBranch = "MAIN";
+        String username = "user";
+        String password = "pwd";
+        String rootPath = "parallelDBVersionCmdRoot/output";
+        String tmpPath = "parallelDBVersionCmdRoot/tmp";
+        String[] schemas = {"AAA", "BBB", "CCC"};
+
+        InMemoryTestRepository repository = new InMemoryTestRepository();
+        repository.fillRepositoryWithSampleDBObjects(schemas);
+        SqlNormalizer sqlNormalizer = SqlNormalizer.getInstance(repository);
+
+        MockDatabaseSerializerFactory factoryPar = new MockDatabaseSerializerFactory();
+        factoryPar.setRepository(repository);
+        factoryPar.setVcs(mock(VersionControlSystem.class));
+
+        JacksonYamlSerializer textSerializer = new JacksonYamlSerializer();
+
+        ParallelDBVersionCommand cmdPar =
+                new ParallelDBVersionCommandBuilder(factoryPar)
+                        .setVCSRemoteInfo(remoteRepoUrl, baseBranch, username, password)
+                        .setWorkspaceInfo(rootPath, tmpPath)
+                        .setDBSchemasToBeSerialized(schemas)
+                        .addDBEnvironment("DEV", "jdbc:dummy:dev", username, password)
+                        .addDBEnvironment("QA", "jdbc:dummy:qa", username, password)
+                        .build();
+
+        cmdPar.takeDatabaseSchemaSnapshotVersion();
+        InMemoryTestDBModelFS outputFSPar = factoryPar.getDBModelFS(tmpPath + "/DEV");
+
+        //create same serializer but now serial
+        MockDatabaseSerializerFactory factorySer = new MockDatabaseSerializerFactory();
+        factorySer.setRepository(repository);
+        factorySer.setVcs(mock(VersionControlSystem.class));
+
+        DBVersionCommand cmdSer = new DBVersionCommand(Arrays.asList(schemas), rootPath, this.vcs );
+        cmdSer.addDBEnvironment(new DBModelSerializerBuilder(factorySer, "DEV", "jdbc:dummy:dev", username, password));
+        cmdSer.addDBEnvironment(new DBModelSerializerBuilder(factorySer, "QA", "jdbc:dummy:qa", username, password));
+
+        cmdSer.takeDatabaseSchemaSnapshotVersion();
+        InMemoryTestDBModelFS outputFSSer = factorySer.getDBModelFS(rootPath);
+
+        for(String schema : schemas){
+            for(Table sourceTable : repository.getInnerTables(schema)){
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoParallel = outputFSPar.getSerializationInfo(sourceTable);
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoSerial = outputFSSer.getSerializationInfo(sourceTable);
+
+                assertEquals(serializedTableInfoParallel.getYamlText(), serializedTableInfoSerial.getYamlText());
+                assertEquals(serializedTableInfoParallel.getDBObject(), serializedTableInfoSerial.getDBObject());
+            }
+
+            for(View sourceView : repository.getInnerViews(schema)){
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoParallel = outputFSPar.getSerializationInfo(sourceView);
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoSerial = outputFSSer.getSerializationInfo(sourceView);
+
+                assertEquals(serializedTableInfoParallel.getYamlText(), serializedTableInfoSerial.getYamlText());
+                assertEquals(serializedTableInfoParallel.getDBObject(), serializedTableInfoSerial.getDBObject());
+            }
+
+            for(Index sourceIndex : repository.getInnerIndexes(schema)){
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoParallel = outputFSPar.getSerializationInfo(sourceIndex);
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoSerial = outputFSSer.getSerializationInfo(sourceIndex);
+
+                assertEquals(serializedTableInfoParallel.getYamlText(), serializedTableInfoSerial.getYamlText());
+                assertEquals(serializedTableInfoParallel.getDBObject(), serializedTableInfoSerial.getDBObject());
+            }
+
+            for(Trigger sourceTrigger : repository.getInnerTriggers(schema)){
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoParallel = outputFSPar.getSerializationInfo(sourceTrigger);
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoSerial = outputFSSer.getSerializationInfo(sourceTrigger);
+
+                assertEquals(serializedTableInfoParallel.getYamlText(), serializedTableInfoSerial.getYamlText());
+                assertEquals(serializedTableInfoParallel.getDBObject(), serializedTableInfoSerial.getDBObject());
+            }
+
+            for(Routine sourceRoutine : repository.getInnerRoutines(schema)){
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoParallel = outputFSPar.getSerializationInfo(sourceRoutine);
+                InMemoryTestDBModelFS.SerializationInfo serializedTableInfoSerial = outputFSSer.getSerializationInfo(sourceRoutine);
+
+                assertEquals(serializedTableInfoParallel.getYamlText(), serializedTableInfoSerial.getYamlText());
+                assertEquals(serializedTableInfoParallel.getDBObject(), serializedTableInfoSerial.getDBObject());
+            }
+
+        }
     }
 }
